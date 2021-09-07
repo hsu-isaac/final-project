285e855f206f6e454dd419d1fe27c6116eef73a6
Isaac Hsu <16hsuisaac@gmail.com>
2021-09-07 15:16:55 -0700

    added database schema

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,47 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public.user" (
+	"userId" serial NOT NULL,
+	"username" TEXT NOT NULL UNIQUE,
+	"firstName" TEXT NOT NULL,
+	"lastName" TEXT NOT NULL,
+	"hashPass" TEXT NOT NULL UNIQUE,
+	CONSTRAINT "user_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.events" (
+	"eventId" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"eventName" TEXT NOT NULL,
+	"dateTime" time with time zone NOT NULL,
+	"description" TEXT NOT NULL,
+	"location" point NOT NULL,
+	"imageUrl" TEXT NOT NULL,
+	CONSTRAINT "events_pk" PRIMARY KEY ("eventId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.invites" (
+	"eventId" integer NOT NULL,
+	"userId" integer NOT NULL,
+	"response" BOOLEAN
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "events" ADD CONSTRAINT "events_fk0" FOREIGN KEY ("userId") REFERENCES "user"("userId");
+
+ALTER TABLE "invites" ADD CONSTRAINT "invites_fk0" FOREIGN KEY ("eventId") REFERENCES "events"("eventId");
+ALTER TABLE "invites" ADD CONSTRAINT "invites_fk1" FOREIGN KEY ("userId") REFERENCES "user"("userId");
