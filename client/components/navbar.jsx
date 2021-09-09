ece18de9b357f93a7ba8494e45a9b97dfc371dbf
Isaac Hsu <16hsuisaac@gmail.com>
2021-09-09 12:37:44 -0700

    working on front end for the event form, created title input and date/time input but have not styled yet

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default class Navbar extends React.Component {
   render() {
     return (
-      <nav>
+      <nav className="footer">
         <div className="container">
-          <div className="iconContainer">
+          <div className="iconContainer row-33 justify-center">
             <img src="images/blue_home.png"/>
             <p>Home</p>
           </div>
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/navbar.jsx';
+import EventForm from '../components/form.jsx';
 
 export default function Home(props) {
   return (
     <>
       <div className="container">
       </div>
+      <EventForm />
       <Navbar />
     </>
   );
