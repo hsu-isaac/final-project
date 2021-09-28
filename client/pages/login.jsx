e4e25c2661ca39daf1b4833a36395c734ac6ffaf
Isaac Hsu <16hsuisaac@gmail.com>
2021-09-28 16:00:43 -0700

    adjusted size on image in info window

--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -3,17 +3,15 @@ import React from 'react';
 export default class Login extends React.Component {
   render() {
     return (
-      <div className="real-container">
-        <div className="single-container column justify-center">
-          <h1 className="title">Hangouts</h1>
-          <img src="/images/people_image.png"></img>
-            <a href="/auth/google">
-              <button className="big-button">Sign In</button>
-            </a>
-            <a href="/auth/demo">
-              <button className="big-button">Demo Sign In</button>
-            </a>
-        </div>
+      <div className="real-container column justify-center">
+        <h1 className="title">Hangouts</h1>
+        <img src="/images/people_image.png"></img>
+          <a href="/auth/google">
+            <button className="big-button">Sign In</button>
+          </a>
+          <a href="/auth/demo">
+            <button className="big-button">Demo Sign In</button>
+          </a>
       </div>
     );
   }
